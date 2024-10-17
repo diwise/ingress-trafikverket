1f50ef7e05b70ada233e9c381496f5b657a7e83a
Isak Styf <isak.styf@masarinconsulting.se>
2024-10-17 14:49:01 +0200

    removed logging from road accident publisher and created a more descriptive id for road accidents

--- internal/pkg/application/services/roadaccidents/publishroadaccidents.go
+++ internal/pkg/application/services/roadaccidents/publishroadaccidents.go
@@ -3,6 +3,7 @@ package roadaccidents
 import (
 	"context"
 	"errors"
+	"fmt"
 	"strconv"
 	"strings"
 	"time"
@@ -11,7 +12,6 @@ import (
 	ngsierrors "github.com/diwise/context-broker/pkg/ngsild/errors"
 	"github.com/diwise/context-broker/pkg/ngsild/types/entities"
 	"github.com/diwise/context-broker/pkg/ngsild/types/entities/decorators"
-	"github.com/diwise/service-chassis/pkg/infrastructure/o11y/logging"
 	"github.com/diwise/service-chassis/pkg/infrastructure/o11y/tracing"
 )
 
@@ -20,35 +20,33 @@ func (ts *roadAccidentSvc) publishRoadAccidentToContextBroker(ctx context.Contex
 	ctx, span := tracer.Start(ctx, "publish-to-broker")
 	defer func() { tracing.RecordAnyErrorAndEndSpan(err, span) }()
 
-	logger := logging.GetFromContext(ctx)
-
 	attributes, err := convertRoadAccidentToFiwareEntity(dev, deleted)
 	if err != nil {
-		logger.Error("failed to create attribute for fiware entity", "err", err.Error())
+		err = fmt.Errorf("failed to create attribute for fiware entity: %s", err.Error())
 		return err
 	}
 
 	fragment, _ := entities.NewFragment(attributes...)
-	entityID := fiware.RoadAccidentIDPrefix + dev.Id
+	entityID := fiware.RoadAccidentIDPrefix + "se:trafikverket:api:deviation:" + dev.Id
 
 	headers := map[string][]string{"Content-Type": {"application/ld+json"}}
 
 	_, err = ts.ctxBroker.MergeEntity(ctx, entityID, fragment, headers)
 	if err != nil {
 		if !errors.Is(err, ngsierrors.ErrNotFound) {
-			logger.Error("failed to merge entity", "err", err.Error())
+			err = fmt.Errorf("failed to merge entity: %s", err.Error())
 			return err
 		}
 
 		entity, err := entities.New(entityID, fiware.RoadAccidentTypeName, attributes...)
 		if err != nil {
-			logger.Error("entities.New failed", "err", err.Error())
+			err = fmt.Errorf("entities.New failed: %s", err.Error())
 			return err
 		}
 
 		_, err = ts.ctxBroker.CreateEntity(ctx, entity, headers)
 		if err != nil {
-			logger.Error("failed to post road accident to context broker", "err", err.Error())
+			err = fmt.Errorf("failed to post road accident to context broker: %s", err.Error())
 			return err
 		}
 	}
